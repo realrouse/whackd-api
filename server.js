bb191cd337b90f379d271e4363dbe0504b3b87ed
nt5k <nicholastysh@gmail.com>
2019-11-29 19:08:51 -0500

    cleanup and update

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
-// port for heroku and default port
 const port = process.env.PORT || 5000;
-
-// now we can use public and data folders
-app.use(express.static(__dirname + '/app/public'));
 
 // middleware
 app.use(express.json());
@@ -17,8 +13,8 @@ app.use(express.json({ type: 'application/*+json' }));
 //routes
 app.use(routes);
 
-// use html and api files instead of putting them here
+// routes.js
 require('./routes');
 
 // listener for console
-app.listen(port, () => console.log(`listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}`));
