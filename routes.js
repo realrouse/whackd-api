4c467416549feb5dcbd8685dca3c623f21ba2e01
admin <admin@admins-MacBook-Pro.local>
2022-04-13 13:44:16 +0200

    update

--- routes.js
+++ routes.js
@@ -8,33 +8,35 @@ const axios = require("axios")
 const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/6c9b0e506a304bc799dc709d0984db04"))
 const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]
 const URL = 'https://etherscan.io/address/0xCF8335727B776d190f9D15a54E6B9B9348439eEE'
-const address = "0xCF8335727B776d190f9D15a54E6B9B9348439eEE" //BitcoinSoV
+const address = "0xCF8335727B776d190f9D15a54E6B9B9348439eEE" //WHACKD contract address
 const contract = new web3.eth.Contract(abi, address)
 const decimalPlaces = 1000000000000000000
 const supply = 1000000000
 const bigSupply = 1000000000000000000000000000
+const name = "Whackd"
+const symbol = "WHACKD"
 const _ = contract.methods
 const parse = input => { return JSON.parse(input) }
 const formatted = input => { return parse(input) / decimalPlaces }
 module.exports = router;
 
 router.get('/api', (__, res) => {
-    _.name().call((__, name) => { 
         _.totalSupply().call((__, totalSupply) => { 
-  _._totalSupply().call((__, _totalSupply) => {
-            _.symbol().call((__, symbol) => { 
+  _._totalSupply().call((__, _totalSupply) => { 
                 _.decimals().call((__, decimals) => { 
                                                                                         const burn = supply - formatted(totalSupply)
-											
+											const bigburn = bigsupply - parse(totalSupply)
                                                                                     res.send({
                                                                                         name,
                                                                                         symbol,
-                                                                                        circulatingSupply: parse(_totalSupply) - (bigSupply - parse(totalSupply)),
-                                                                                        circulatingSupplyFormatted: formatted(_totalSupply) - totalSupply,
+                                                                                        circulatingSupply: parse(_totalSupply) - bigburn,
+                                                                                        circulatingSupplyFormatted: formatted(_totalSupply) - burn,
                                                                                         totalSupply: parse(totalSupply),
-											
+												
+										
                                                                                         totalSupplyFormatted: formatted(totalSupply),
-                                                                                  
+                                                                                	maxSupplyFormatted: parse(supply),
+											maxSupply: parse(bigsupply) 
                                                                                         tokensBurned: bigSupply - parse(totalSupply),
                                                                                         tokensBurnedFormatted: burn,
                                                                                         tokensBurnedPercent: burn / supply,
