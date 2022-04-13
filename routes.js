32e25040b8a3359b315cd14cfb8fa843997728e9
admin <admin@admins-MacBook-Pro.local>
2022-04-13 12:24:02 +0200

    update

--- routes.js
+++ routes.js
@@ -57,9 +57,9 @@ router.get('/circulating-text', (__, res) => {
     _.totalSupply().call((__, totalSupply) => { 
         _._totalSupply().call((__, _totalSupply) => {
             const burn = supply - formatted(totalSupply)
-            res.send(
+            res.send({
                 formatted(_totalSupply) - burn
-            )
+            })
         })
     })
 });
