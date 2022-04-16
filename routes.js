1e14ff2df535c16b2fbb76bd21b13c011169bd22
admin <admin@admins-MacBook-Pro.local>
2022-04-16 13:02:16 +0200

    update

--- routes.js
+++ routes.js
@@ -107,8 +107,18 @@ router.get('/circulating', (__, res) => {
         console.log(response.data.result)
        const {result} = response.data
         res.send({
-            timeLockedTokens: parse(result),
-            timeLockedTokensFormatted: formatted(result)
+            UniswapWhackdEthAmountOfEth: formatted(result)
+        })
+    })
+ });
+
+ router.get('/liquidity-whackd', (__, res) => {
+    const UniswapWhackdEthAmountOfWhackd = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xCF8335727B776d190f9D15a54E6B9B9348439eEE&address=0xc491405d542a393d8d202a72f0fb076447e61891&tag=latest&apikey=VFXCIKBK33QVIW42AM7153EANCA3YT7Q7V';
+    axios.get(UniswapWhackdEthAmountOfWhackd).then(function (response) {
+        console.log(response.data.result)
+       const {result} = response.data
+        res.send({
+            UniswapWhackdEthAmountOfWhackd: formatted(result)
         })
     })
  });
